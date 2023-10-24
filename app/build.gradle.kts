66bbc18aee875cab09ec4f3ac2260d05ad7e8f07
quessr <heosera76@gmail.com>
2023-10-24 17:02:36 +0900

    feat: dataBinding, viewBinding 세팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    dataBinding {
+        enable = true
+    }
+
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
@@ -39,4 +47,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // data binding
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
 }
