23f4cfb7289cf2f87d782d890891d747d9d1cdbb
quessr <heosera76@gmail.com>
2023-10-25 11:44:54 +0900

    feat: MainActivity에 RunFragment 연결

--- app/src/main/java/com/example/running_app/ui/fragments/RunFragment.java
+++ app/src/main/java/com/example/running_app/ui/fragments/RunFragment.java
@@ -1,6 +1,25 @@
 package com.example.running_app.ui.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.running_app.databinding.FragmentRunBinding;
+
 public class RunFragment extends Fragment {
+    private FragmentRunBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentRunBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        return view;
+    }
 }
