b8ffc49c8a5e5af64424dbd4485d5dd1386e2916
heojuseon <hjs2683@naver.com>
2023-11-22 09:53:11 +0900

    상세화면 gps 정보 없을경우 수정

--- app/src/main/java/com/example/running_app/data/database/dao/GpsDao.java
+++ app/src/main/java/com/example/running_app/data/database/dao/GpsDao.java
@@ -9,13 +9,11 @@ import java.util.List;
 //Data Access Object
 @Dao
 public interface GpsDao {
+    //삽입
     @Insert
-        //삽입
     void setInsertGps(TB_GPS tb_gps);
     //조회 쿼리
-
     @Query("SELECT * FROM TB_GPS")
-    //쿼리 : 데이터베이스에 요청하는 명령문
     List<TB_GPS> getGpsAll();
 
     //전체 좌표 조회
