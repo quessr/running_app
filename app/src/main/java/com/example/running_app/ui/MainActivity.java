d78d8f3feba3cd3f48834f01bf8ad228a9da9244
quessr <heosera76@gmail.com>
2023-11-24 16:10:57 +0900

    feat: persistent bottom sheet dialog 구현
    - bottom sheet를 드래그하여 아래로 내릴수 있게하는 (bottom sheet의 높이를 낮추는) 기능 구현

--- app/src/main/java/com/example/running_app/ui/MainActivity.java
+++ app/src/main/java/com/example/running_app/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.running_app.ui;
 
 import static com.example.running_app.data.model.PolylineUpdater.clearPolyline;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -20,6 +21,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 
@@ -32,6 +34,7 @@ import com.example.running_app.ui.fragments.MainHistoryFragment;
 import com.example.running_app.ui.fragments.RunFragment;
 import com.example.running_app.ui.viewmodels.RunViewModel;
 import com.example.running_app.ui.viewmodels.TimerViewModel;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager = getSupportFragmentManager();
@@ -44,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
     //timer, room DB
     private TimerViewModel timerViewModel;
+    private float initialY;
 
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
@@ -53,6 +57,44 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        View bottomSheet = binding.bottomSheet;
+
+        BottomSheetBehavior<View> sheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        sheetBehavior.setHideable(true);
+        sheetBehavior.setSkipCollapsed(false);
+
+        // Bottom Sheet의 높이 조절을 위한 터치 핸들러 설정
+        bottomSheet.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                initialY = event.getRawY();
+            } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                float dy = event.getRawY() - initialY;
+                float newOffset = Math.max(100, sheetBehavior.getPeekHeight() - dy);
+                sheetBehavior.setPeekHeight((int) newOffset);
+                initialY = event.getRawY();
+            }
+            return true;
+        });
+
+        sheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        sheetBehavior.setPeekHeight(100); // 최소 높이 설정
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED); // 숨겨진 상태일 때는 COLLAPSED 상태로 변경
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        });
+        sheetBehavior.setPeekHeight(800);
 
         //timer - viewModel
         timerViewModel = new ViewModelProvider(this).get(TimerViewModel.class);
@@ -155,6 +197,7 @@ public class MainActivity extends AppCompatActivity {
             runFragment.clearMarkersAndCircle();
         }
     }
+
     ServiceConnection serviceGpsTrackerConnection = new ServiceConnection() {
         @Override
         public void onServiceDisconnected(ComponentName name) {
