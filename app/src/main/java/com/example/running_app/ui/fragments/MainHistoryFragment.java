a574c938468ee2f4e55f911a597ed309378a2c4c
quessr <heosera76@gmail.com>
2023-11-24 17:56:06 +0900

    refactor: 로직 수정 및 주석 삭제

--- app/src/main/java/com/example/running_app/ui/fragments/MainHistoryFragment.java
+++ app/src/main/java/com/example/running_app/ui/fragments/MainHistoryFragment.java
@@ -3,9 +3,7 @@ package com.example.running_app.ui.fragments;
 import static com.example.running_app.ui.MainActivity.bottomSheet;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +15,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.running_app.R;
-import com.example.running_app.ui.MainActivity;
 
 public class MainHistoryFragment extends Fragment {
 
@@ -54,15 +51,12 @@ public class MainHistoryFragment extends Fragment {
         OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
+
                 if (bottomSheet.getVisibility() != View.VISIBLE) {
                     bottomSheet.setVisibility(View.VISIBLE);
-                    requireActivity().getSupportFragmentManager().popBackStack();
-                    viewFind();
-                } else {
-                    requireActivity().getSupportFragmentManager().popBackStack();
-                    viewFind();
                 }
-
+                requireActivity().getSupportFragmentManager().popBackStack();
+                viewFind();
             }
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(this, onBackPressedCallback);
